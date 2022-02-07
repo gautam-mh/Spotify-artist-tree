a7aaee01c796daec23fefa7cb3fccaa961c872a9
Gautam M H <gautammhiremath@gmail.com>
2022-02-07 16:32:39 +0530

    final result 1

--- spotify_artist_tree/main.py
+++ spotify_artist_tree/main.py
@@ -3,28 +3,38 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import cred
 
 client_credentials_manager = SpotifyClientCredentials(
-    client_id=cred.client_ID, client_secret=cred.client_SECRET)
+    client_id=cred.client_ID, client_secret=cred.client_SECRET
+)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
 def get_artist_id(artist_name):
-    artist = sp.search(f"name:{artist_name}&type=artist")
-    return artist
+    data = sp.search(q=f"artist:{artist_name}", type="artist")
+    return data["artists"]["items"][0]["id"]
 
 
 def get_albums(artist_id):
-    album = sp.artist_albums(artist_id, album_type="album")
-    return album
+    album_data = sp.artist_albums(artist_id, album_type="album")
+    return album_data["items"]
 
 
 def get_tracks(album_id):
-    tracks = sp.album_tracks(album_id)
-    return tracks
+    tracks_data = sp.album_tracks(album_id)
+    return tracks_data["items"]
 
 
-get_albums(
-    "https://open.spotify.com/artist/36QJpDe2go2KgaRleHCDTp?si=L_nga3lOT02AkK2YN8mCLA")
-get_tracks(
-    "https://open.spotify.com/album/6VH2op0GKIl3WNTbZmmcmI?si=1XxL9nRaQrOm2im6MDayPA")
+# print(get_tracks("6VH2op0GKIl3WNTbZmmcmI"))
 
-print(get_artist_id("Led Zeppelin"))
+# # print(get_albums("36QJpDe2go2KgaRleHCDTp"))
+
+# print(get_artist_id("Led Zeppelin"))
+
+artist = input()
+artist_id = get_artist_id(artist)
+albums = get_albums(artist_id)
+for album in albums:
+    print(album["release_date"][0:4])
+    print("  |--" + album["name"])
+    tracks = get_tracks(album["id"])
+    for track in tracks:
+        print("    |--" + track["name"])
